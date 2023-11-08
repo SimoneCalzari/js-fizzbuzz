6f938c78bd5ea44a919d12c49fe28b6bb8b81681
SimoneCalzari <simone.calzari@outlook.it>
2023-11-08 15:17:47 +0100

    feat: bonus 1

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 'use strict';
-
+// CICLO FOR CONTATORE
 for (let pippo = 1; pippo <= 100; pippo++) {
+  // mi salvo l'elemento container padre
+  const divPadre = document.getElementById('container-boxes');
+  const divFiglio = document.createElement('div');
 
-  if (pippo % 3 === 0 && pippo % 5 === 0 ) {
+  if (pippo % 3 === 0 && pippo % 5 === 0 ) /* DIV. PER 3 E 5 */ {
     console.log('FizzBuzz');
+    divFiglio.append('FizzBuzz');
     
-  } else if (pippo % 3 === 0 && pippo % 5 !== 0 ) {
+  } else if (pippo % 3 === 0 && pippo % 5 !== 0 ) /* DIV. SOLO PER 3 */ {
     console.log('Fizz');
-    
-  } else if (pippo % 3 !== 0 && pippo % 5 === 0) {
-    console.log('Buzz');
+    divFiglio.append('Fizz');
 
-  } else {
+    
+  } else if (pippo % 3 !== 0 && pippo % 5 === 0) /* DIV. SOLO PER 5 */ {
+    console.log('Buzz');
+    divFiglio.append('Buzz');
+
+
+  } else /* CASI RESTANTI */ {
     console.log(pippo);
+    divFiglio.append(pippo);
 
   }
+
+  divPadre.append(divFiglio);
 }
