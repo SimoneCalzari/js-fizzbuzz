bcd79812e757ecb0cc4834d5ac76a7c0b4cbd03d
SimoneCalzari <simone.calzari@outlook.it>
2023-11-09 10:43:37 +0100

    fix: ottimizzazione codice

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 'use strict';
+
+const divPadre = document.getElementById('container-boxes'); // PRENDO DIV PADRE E LO SALVO IN UNA COSTANTE
+
 // CICLO FOR CONTATORE
 for (let pippo = 1; pippo <= 100; pippo++) {
 
-  const divPadre = document.getElementById('container-boxes'); // PRENDO DIV PADRE E LO SALVO IN UNA COSTANTE
   const divFiglio = document.createElement('div');  // CREO DIV FIGLI E SALVO IN COSTANTE
   const divFiglioContenuto = document.createElement('div'); // CREO DIV CONTENUTO DEI FIGLI E SALVO IN COSTANTE
 
-  if (pippo % 3 === 0 && pippo % 5 === 0 ) /* DIV. PER 3 E 5 */ {
+  if (pippo % 3 === 0 && pippo % 5 === 0) /* DIV. PER 3 E 5 */ {
     console.log('FizzBuzz'); // SU CONSOLE 
     divFiglioContenuto.append('FizzBuzz'); // INSERISCO TESTO DENTRO DIV CONTENUTO
     divFiglio.classList.add('fizz-buzz'); // AGGIUNGO CLASSE AL DIV FIGLIO PER CAMBIARE STILE
     
-  } else if (pippo % 3 === 0 && pippo % 5 !== 0 ) /* DIV. SOLO PER 3 */ {
+  } else if (pippo % 3 === 0) /* DIV. SOLO PER 3 */ {
     console.log('Fizz'); // SU CONSOLE
     divFiglioContenuto.append('Fizz'); // INSERISCO TESTO DENTRO DIV CONTENUTO
     divFiglio.classList.add('fizz'); // AGGIUNGO CLASSE AL DIV FIGLIO PER CAMBIARE STILE
     
-  } else if (pippo % 3 !== 0 && pippo % 5 === 0) /* DIV. SOLO PER 5 */ {
+  } else if (pippo % 5 === 0) /* DIV. SOLO PER 5 */ {
     console.log('Buzz'); // SU CONSOLE
     divFiglioContenuto.append('Buzz'); // INSERISCO TESTO DENTRO DIV CONTENUTO
     divFiglio.classList.add('buzz'); // AGGIUNGO CLASSE AL DIV FIGLIO PER CAMBIARE STILE
